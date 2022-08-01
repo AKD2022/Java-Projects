bb7f1dfbdb16b72f6cbabccf94853930d1c8e53d
unknown <akdusa20@gmail.com>
2022-08-01 16:04:29 -0400

    started project

---
+++ JavaJobs.java
@@ -0,0 +1,13 @@
+public class JavaJobs {
+    public static void main(String[] args) {
+        System.out.println("********************************");
+        System.out.println("************JAVA JOBS **********");
+        System.out.println("********************************\n");
+
+        System.out.println("Welcome to JAVA Jobs. What would you like to do?");
+        System.out.println("\t- Apply for a job (a)");
+        System.out.println("\t- Work a shift (b)");
+        System.out.println("\t- Ask for a raise (c)");
+        System.out.println("\t- Learn about Java (d)");
+    }
+}
