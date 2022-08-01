25ca3e7a0f33698da3c8ce581faf40e5948a9b69
unknown <akdusa20@gmail.com>
2022-08-01 16:28:19 -0400

    commit

--- JavaJobs.java
+++ JavaJobs.java
@@ -21,6 +21,7 @@ public class JavaJobs {
                 Boolean allowed = applyJobAllowed();
                 if (allowed = true){
                     apply();
+                    
                 }
             
                 break;
