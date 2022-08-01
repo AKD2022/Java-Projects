17218c94b28acdb8fa38d87a1978bb575f169ef9
unknown <akdusa20@gmail.com>
2022-08-01 16:07:04 -0400

    commit

--- JavaJobs.java
+++ JavaJobs.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class JavaJobs {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("********************************");
         System.out.println("************JAVA JOBS **********");
         System.out.println("********************************\n");
