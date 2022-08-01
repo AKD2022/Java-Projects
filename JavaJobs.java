14741139dd3c4671d0b125cc04d84754ec90673b
unknown <akdusa20@gmail.com>
2022-08-01 16:06:09 -0400

    commit

--- JavaJobs.java
+++ JavaJobs.java
@@ -5,9 +5,11 @@ public class JavaJobs {
         System.out.println("********************************\n");
 
         System.out.println("Welcome to JAVA Jobs. What would you like to do?");
-        System.out.println("\t- Apply for a job (a)");
-        System.out.println("\t- Work a shift (b)");
-        System.out.println("\t- Ask for a raise (c)");
-        System.out.println("\t- Learn about Java (d)");
+        System.out.println("\t- Apply for a job (type a)");
+        System.out.println("\t- Work a shift (type b)");
+        System.out.println("\t- Ask for a raise (type c)");
+        System.out.println("\t- Learn about Java (type d)");
+
+        
     }
 }
