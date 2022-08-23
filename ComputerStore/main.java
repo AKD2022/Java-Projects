79565b75e579ca7d43e81653c111cd3765af6d5c
unknown <akdusa20@gmail.com>
2022-08-23 17:44:42 -0400

    ran file

--- ComputerStore/main.java
+++ ComputerStore/main.java
@@ -1,3 +1,9 @@
-public class Main {
+import models.Computers;
 
+public class Main {
+    public static void main(String[] args) {
+        Computers computer = new Computers("Dell XPS 15", 2021, "Windows 11", 1599.99);
+
+        System.out.println(computer);
+    }
 }
--- ComputerStore/models/Computers.java
+++ ComputerStore/models/Computers.java
@@ -1,5 +1,50 @@
 package models;
 
 public class Computers {
+    private String model;
+    private int year;
+    private String OS;
+    private double price;
+
+    public Computers(String model, int year, String OS, double price) {
+        this.model = model;
+        this.year = year;
+        this.OS = OS;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getOS() {
+        return OS;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setOS(String oS) {
+        OS = oS;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     
+
 }
