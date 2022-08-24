32610e292bd7c17b77238933cfc5d751fad4d845
unknown <akdusa20@gmail.com>
2022-08-24 12:54:04 -0400

    commit

--- ComputerStore/models/ComputerStore.java
+++ ComputerStore/models/ComputerStore.java
@@ -1,5 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+
 public class ComputerStore {
-    
+    ArrayList<Computers> computer;
+
+    public ComputerStore() {
+        this.computer = new ArrayList<Computers>();
+    }
+
+    public Computers getMovie(int index) {
+        return new Computers(this.computer.get(index));
+    }
+
 }
--- ComputerStore/models/Computers.java
+++ ComputerStore/models/Computers.java
@@ -1,5 +1,6 @@
 package models;
 
+
 public class Computers {
     private String model;
     private int year;
@@ -7,11 +8,22 @@ public class Computers {
     private double price;
 
     public Computers(String model, int year, String OS, double price) {
+        if (model.isBlank() || model == null) {
+            throw new IllegalArgumentException("model cannot be blank/null");
+        } else if (OS.isEmpty()) {
+            throw new IllegalArgumentException("OS cannot be empty");
+        } else if (price < 0) {
+            throw new IllegalArgumentException("price cannot be below 0");
+        } else if (year < 0) {
+            throw new IllegalArgumentException("INVALID year");
+        }
+
         this.model = model;
         this.year = year;
         this.OS = OS;
         this.price = price;
     }
+
 
     public String getModel() {
         return model;
@@ -31,20 +43,37 @@ public class Computers {
 
     public void setModel(String model) {
         this.model = model;
+        if (model.isBlank() || model == null) {
+            throw new IllegalArgumentException("model cannot be blank/null");
+        }
     }
 
     public void setOS(String oS) {
         OS = oS;
+        if (OS.isEmpty()) {
+            throw new IllegalArgumentException("OS cannot be empty");
+        }
     }
 
     public void setPrice(double price) {
         this.price = price;
+        if (price < 0) {
+            throw new IllegalArgumentException("price cannot be below 0");
+        }
     }
 
     public void setYear(int year) {
         this.year = year;
+        if (year < 0) {
+            throw new IllegalArgumentException("INVALID year");
+        }
     }
 
-    
+    public String toString() {
+        return "\nModel: " + this.model +
+        "\nOperating System (OS): " + this.OS + 
+        "\nPrice: $" + this.price + 
+        "\nYear: "  + this.year;
+    }
 
 }
