abeb72d944e0590106a66f7ec8d8064ee97e8023
AKD2022 <akdusa20@gmail.com>
2023-01-07 18:17:03 -0500

    started

--- Student_Management_System/Student.java
+++ Student_Management_System/Student.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 public class Student {
-    
+    private String firstName;
+        private String lastName;
+        private int gradeYear;
+        private int studentID;
+        private String courses;
+        private int tuitionBalance;
+        private static int costOfCourse = 600;
+        private static int id = 1001;
+    // Constructor: prompt user to enter student's name and year
+    public Student() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Student first name: ");
+        this.firstName = scan.nextLine();
+
+        System.out.println("Enter Student last name: ");
+        this.lastName = scan.nextLine();
+
+        System.out.println("Enter Student class level:1\n - for Freshmen\n2 - for Sophmore\n3 - for Junior\n4 - for Senior\n");
+        this.gradeYear = scan.nextInt();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+    }
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance 
+
+    // Pay tuition
+
+    // Show status
 }
--- Student_Management_System/StudentDatabase.java
+++ Student_Management_System/StudentDatabase.java
@@ -1,5 +1,8 @@
 public class StudentDatabase {
     public static void main(String[] args) {
-        
+        Student stu1 = new Student();
+        // Ask how many users we want to add
+
+        // Create n number of students
     }
 }
