116025f2721f1fff112f07952e7dff91bc227622
AKD2022 <akdusa20@gmail.com>
2023-01-16 19:17:15 -0500

    created files

--- ComputerStore/main.java
+++ ComputerStore/main.java
@@ -3,12 +3,12 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import models.Computer;
-import models.Store;
+import models.OnlineStore;
 
 public class Main {
     
 
-    static Store store = new Store();
+    static OnlineStore store = new OnlineStore();
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("\n********************JAVA COMPUTER STORE ********************\n");
